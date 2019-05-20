7366ba9f3f9d6bad857e6057c79e7a3a2b657754
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-05-20 00:33:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each do |key,value|   #key is :color, value is {:purple => [...]}
   	value.each do |attribute,names| #attribute is :purple, names is [...]
   		names.each do |pigeon| #pigeon is "Theo"
-  			result[pigeon] = {} #{"Theo": }
-  			result[pigeon][key] = [] #{"Theo" : [...]}
+  			result[pigeon] ||= {} #{"Theo": }
+  			result[pigeon][key] ||= [] #{"Theo" : [...]}
   			result[pigeon][key] << attribute.to_s
   		end
   	end
